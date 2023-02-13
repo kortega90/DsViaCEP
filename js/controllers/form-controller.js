1469f60b68a8e000f3b6aae0fc37f58d1cf0f3ca
Kengi Ortega <kortega90@hotmail.com>
2023-02-13 20:54:31 -0300

    quart

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -30,8 +30,29 @@ export function init(){
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
     
-    setFormError("cep", "este é a mensagem do CEP")
-    setFormError("number", "este é a mensagem do CEP")
+   state.inputNumber.addEventListener('change', handleInputNumberChange);
+}
+function handleInputNumberChange (event){
+  if(event.target.value == ""){
+    setFormError("number","Campo requerido");
+  }
+  else{
+    setFormError("number","");
+  }
+}
+function handleBtnClearClick(event){
+    event.preventDefault();
+    clearForm();
+
+}
+function clearForm(){
+  state.inputCep.value = null;
+  state.inputCity.value = null;
+  state.inputNumber.value = null;
+  state.inputStreet.value = null;
+
+  setFormError("cep", null)
+  setFormError("number", null)
 }
 function setFormError (key, value){
     const element = document.querySelector(`[data-error="${key}"]`);
